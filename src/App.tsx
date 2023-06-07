c1eed8b64aa3937ea130ed7b21c01cf6d458e772
Felipe Mellão <felipe.mellao08@gmail.com>
2023-06-07 19:43:31 -0300

    Fim do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,62 @@
-import { useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import Card from './components/Card';
 import './App.css';
 
+export type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 function App() {
+  const [todoInput, setTodoInput] = useState('');
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const storedTodos = localStorage.getItem('@codersList:todos');
+
+    if (storedTodos) {
+      return JSON.parse(storedTodos)
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@codersList:todos', JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo() {
+    setTodos((previousTodos) => 
+      [...previousTodos, { id: Math.random(), title: todoInput, completed: false }]
+    );
+
+    setTodoInput('');
+  }
+
+  function completeTodo(id: number) {
+    setTodos((previousTodos) => 
+      previousTodos.map((todo) => todo.id !== id ?  todo :  { ...todo, completed: !todo.completed }));
+  }
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    setTodoInput(e.target.value);
+  }
+
+  function deleteTodo(id: number) {
+    setTodos((previousTodos) => previousTodos.filter((todo) => todo.id !== id));
+  }
+
   return (
     <div className="App">
-      <div>
-        <input placeholder="Fazer café" />
-        <button>Adicionar</button>
+      <div className='add-todo'>
+        <input placeholder="Fazer café" value={todoInput} onChange={handleInputChange} />
+        <button onClick={addTodo}>Adicionar</button>
       </div>
 
-      <Card title="Fazer café" />
+      {
+        todos.map((todo) => (
+          <Card key={todo.id} todo={todo} completeTodo={completeTodo} deleteTodo={deleteTodo} />
+        ))
+      }
     </div>
   )
 }
